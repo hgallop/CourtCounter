a78907284366c7c966c8e885b09d7db566f8e291
Heather Gallop <doyouseeitmyway@gmail.com>
2018-01-29 14:34:48 -0500

    Add global integer scoreTeamB. Add team b button score methods. Update Buttons to correct methods.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +24,58 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void threePoints(View view) {
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Calculates update for three point score. Displays updated score.
+     */
+    public void threePointsA(View view) {
         int score = 3;
         scoreTeamA = scoreTeamA + score;
         displayForTeamA(scoreTeamA);
     }
 
-    public void twoPoints(View view) {
+    public void threePointsB(View view) {
+        int score = 3;
+        scoreTeamB = scoreTeamB + score;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    /**
+     * Calculates update for two point score. Displays updated score.
+     */
+    public void twoPointsA(View view) {
         int score = 2;
         scoreTeamA = scoreTeamA + score;
         displayForTeamA(scoreTeamA);
     }
 
-    public void onePoint(View view) {
+    public void twoPointsB(View view) {
+        int score = 2;
+        scoreTeamB = scoreTeamB + score;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    /**
+     * Calculates update for one point score. Displays updated score.
+     */
+    public void onePointA(View view) {
         int score = 1;
         scoreTeamA = scoreTeamA + score;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void onePointB(View view) {
+        int score = 1;
+        scoreTeamB = scoreTeamB + score;
+        displayForTeamB(scoreTeamB);
     }
 }
