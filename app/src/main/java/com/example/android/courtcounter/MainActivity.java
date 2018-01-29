07c50cad07aec96e91556ae056aec59b4cdcc767
Heather Gallop <doyouseeitmyway@gmail.com>
2018-01-29 13:13:55 -0500

    Add method for displaying team a score. Add id to score TextView. Add method to change score display when button clicked.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void threePoints(View view) {
+        int score = 3;
+        displayForTeamA(score);
+    }
+
+    public void twoPoints(View view) {
+        int score = 2;
+        displayForTeamA(score);
+    }
+
+    public void onePoint(View view) {
+        int score = 1;
+        displayForTeamA(score);
     }
 }
