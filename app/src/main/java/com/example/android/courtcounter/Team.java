b6be92744a928d532d3d4434ac513325b4529b39
Heather Gallop <doyouseeitmyway@gmail.com>
2018-03-17 16:21:35 -0400

    Update Team class to resolve lint warnings. Update comment. Make final variables static. Make constructor package private. Make scoring methods package-private. Make get and set score methods package-private.

--- app/src/main/java/com/example/android/courtcounter/Team.java
+++ app/src/main/java/com/example/android/courtcounter/Team.java
@@ -1,26 +1,22 @@
 package com.example.android.courtcounter;
 
-import android.view.Display;
-import android.view.View;
-import android.widget.TextView;
-
 /**
- * Created by doyou on 2/10/2018.
+ * Create Team class
  */
 
 public class Team {
 
     //create constants for scoring
-    private final int THREE_POINTS = 3;
-    private final int TWO_POINTS = 2;
-    private final int ONE_POINT = 1;
+    private final static int THREE_POINTS = 3;
+    private final static int TWO_POINTS = 2;
+    private final static int ONE_POINT = 1;
 
     //create variables for object
     private String mTeamName;
     private int mScore;
 
     //constructor for Team object
-    public Team(String teamName, int score) {
+    Team(String teamName, int score) {
         mTeamName = teamName;
         mScore = score;
     }
@@ -36,27 +32,27 @@ public class Team {
     }
 
     //mutator. allows score to be set.
-    public void setScore(int score){
+    void setScore(int score){
         mScore = score;
     }
 
     //accessor. allows score to be accessed.
-    public int getScore(){
+    int getScore(){
         return mScore;
     }
 
     //moethod for scoring three points
-    public void threePoints() {
+    void threePoints() {
         mScore += THREE_POINTS;
     }
 
     //method for scoring two points.
-    public void twoPoints() {
+    void twoPoints() {
         mScore += TWO_POINTS;
     }
 
     //method for scoring one point.
-    public void onePoint() {
+    void onePoint() {
         mScore += ONE_POINT;
     }
 
